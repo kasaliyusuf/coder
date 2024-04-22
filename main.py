ff52fed85a6ac199f4d38b9814c1b03469f1f6ae
kasaliyusuf <119871066+kasaliyusuf@users.noreply.github.com>
2024-04-22 15:14:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import streamlit as st
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import torch
 
-    st.title("Python Code Generation App")
+st.title("Python Code Generation App")
 
-   
-    st.subheader("Instructions")
-    st.write("Use the following format to enter prompts: Write python code for SBERT vector embedding of a sentence")
-    st.write("")    
+st.subheader("Instructions")
+st.write("Use the following format to enter prompts: Write python code for SBERT vector embedding of a sentence")
+st.write("")    
    
 
