6aa60e30df74b0ad0927e815466f600b82c57d52
kasaliyusuf <119871066+kasaliyusuf@users.noreply.github.com>
2024-04-22 15:12:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,28 +4,9 @@ import torch
 
     st.title("Python Code Generation App")
 
-    # Load the model and tokenizer
-    tokenizer = AutoTokenizer.from_pretrained("Salesforce/codet5p-770m-py")
-    device = 'cpu'
-    model = AutoModelForSeq2SeqLM.from_pretrained("Salesforce/codet5p-770m-py").to(device)
-
-    # Get user input
+   
     st.subheader("Instructions")
     st.write("Use the following format to enter prompts: Write python code for SBERT vector embedding of a sentence")
     st.write("")    
-    query = st.text_input("Enter a prompt here: ")
-    if st.button("Generate Code"):
-        if query.strip().lower() == 'exit':
-            st.stop()
-        else:
-            # Generate summary
-            inputs = tokenizer(f"summarize:{query}", return_tensors="pt")
-            inputs = {k: v.to(device) for k, v in inputs.items()}
-            output = model.generate(**inputs, max_length=750)
-            generated_text = tokenizer.decode(output[0]).replace("summarize:", "")
-
-            # Display the generated summary
-            st.subheader("Generated Code:")
-            st.code(generated_text)
-
+   
 
